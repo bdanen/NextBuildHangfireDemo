43c11daa40e4ead65040216097913c01ff4ce3b5
Bas Danen <bdanen@gmail.com>
2016-04-15 17:51:43 +0200

    Added hangfire package and configured dashboard.

--- NextBuildHangfireDemo/Startup.cs
+++ NextBuildHangfireDemo/Startup.cs
@@ -4,11 +4,18 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(NextBuildHangfireDemo.Startup))]
 namespace NextBuildHangfireDemo
 {
+    using Hangfire;
+
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("MailerDb");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
         }
     }
 }
